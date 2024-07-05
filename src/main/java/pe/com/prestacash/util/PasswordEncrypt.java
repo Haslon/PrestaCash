d8f3d4e4babe40fbc8b0b35ddc10cbc96b816308
Esclender <elesclenderlugo@gmail.com>
2024-07-05 07:58:54 -0500

    seguridad 2

---
+++ src/main/java/pe/com/prestacash/util/PasswordEncrypt.java
@@ -0,0 +1,9 @@
+package pe.com.prestacash.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncrypt {
+  public static void main(String[] args) {
+    System.out.println(new BCryptPasswordEncoder().encode("1234"));
+	}
+}
